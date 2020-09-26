657855e30613c16486f3eb97c794a0d1371018c4
imrptsngh <imrptsngh@gmail.com>
2020-09-27 01:38:53 +0530

    modified admin view (django-admin)

--- blog_app/posts/admin.py
+++ blog_app/posts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
 
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    list_display_links = ['title', 'date']
+    search_fields = ['title', 'content']
+
+
+admin.site.register(Post, PostAdmin)
--- blog_app/posts/models.py
+++ blog_app/posts/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateField(auto_now_add=True)
     content = models.TextField()
+
+    def __str__(self):
+        return self.title
